680f49e795fdd04bf813f773bfb8b23d791baa33
Sepehr Parirokh <sepehrparirokh@gmail.com>
2023-07-15 10:01:56 -0400

    feat: setup logger

---
+++ config/default.ts
@@ -0,0 +1,5 @@
+export default {
+    port: 5000,
+    dbUri: "mongodb://localhost:27017/auth-api",
+    logLevel: 'info'
+};
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('hello world');
+require("dotenv").conf();
+import express from "express";
+import config from "config";
+import { connect } from "mongoose";
+import connectToDb from "./utils/dbConnection";
+import log from "./utils/logger";
+const app = express();
+
+const port = config.get("port");
+
+app.listen(port, () => {
+    log.info(`App started at http://localhost:${port}`),
+
+  connectToDb();
+
+});
+
